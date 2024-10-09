79842f8e35b74a86a4f4ee0369ef80d26a466b31
Nguyen Chi Quoc <nguyenchiquocit@gmail.com>
2024-10-09 22:56:39 +0700

    create project

---
+++ src/main/java/com/itbk/Outlet_Store/OutletStoreApplication.java
@@ -0,0 +1,13 @@
+package com.itbk.Outlet_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutletStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutletStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itbk/Outlet_Store/OutletStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itbk.Outlet_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutletStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
